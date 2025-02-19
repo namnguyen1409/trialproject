486e47fb689d54a439455c74532f915a10ce68a6
Nguyen Van Nam <namnvhe180863@fpt.edu.vn>
2025-02-19 22:05:15 +0700

    fix bug at InvoiceDTO

--- src/main/java/com/hsf302/trialproject/dto/InvoiceDTO.java
+++ src/main/java/com/hsf302/trialproject/dto/InvoiceDTO.java
@@ -1,11 +1,15 @@
 package com.hsf302.trialproject.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+@AllArgsConstructor
+@Builder
 @Data
 @NoArgsConstructor
 public class InvoiceDTO {
@@ -20,16 +24,4 @@ public class InvoiceDTO {
     private LocalDate invoiceDate;
     private boolean isShipped;
 
-    public InvoiceDTO(Long id, String type, BigDecimal totalPrice, BigDecimal totalDiscount, BigDecimal totalPaid, BigDecimal totalDebt, Long customerId, String description, LocalDate invoiceDate, boolean isShipped) {
-        this.id = id;
-        this.type = type;
-        this.totalPrice = totalPrice;
-        this.totalDiscount = totalDiscount;
-        this.totalPaid = totalPaid;
-        this.totalDebt = totalDebt;
-        this.customerId = customerId;
-        this.description = description;
-        this.invoiceDate = invoiceDate;
-        this.isShipped = isShipped;
-    }
 }
